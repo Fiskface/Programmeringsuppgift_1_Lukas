a905744136d4d46145c78248aeed77595483407f
Lukas <lukas.almgren03@gmail.com>
2023-03-29 17:45:52 +0200

    Initial commit

---
+++ Assets/GetPlaneValues.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPlaneValues : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        SceneManager sceneManager = GameObject.FindWithTag("SceneManager").GetComponent<SceneManager>();
+        transform.
+    }
+}
---
+++ Assets/SceneManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public Vector3 ballPosition;
+    public Vector3 ballDirection;
+    public float ballRadius;
+
+    public float planeDistance;
+    public Vector3 planeRotation;
+}
